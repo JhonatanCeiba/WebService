e5e3dc50eb11f2eb7661c08f451496e1a960b7b8
jhonatan.giraldo <jhonatan.giraldo@jhonatan-giraldo.ceiba.local>
2016-07-29 11:36:21 -0500

    Primera version de Web Service

---
+++ src/me/jhonatan/servicio/SimpleRestService.java
@@ -0,0 +1,101 @@
+package me.jhonatan.servicio;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Produces;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+@Path("/service")
+public class SimpleRestService {
+
+	private static final Logger logger = Logger.getLogger(SimpleRestService.class);
+	UserDao userDao = new UserDao();
+	   private static final String SUCCESS_RESULT="<result>success</result>";
+	   private static final String FAILURE_RESULT="<result>failure</result>";
+
+	@GET
+	   @Path("/users")
+	   @Produces(MediaType.APPLICATION_XML)
+	 public ArrayList<User> getUsers(){
+	    return userDao.getAllUsers();
+	 }
+
+	@GET
+	   @Path("/getIds")
+	   @Produces(MediaType.TEXT_PLAIN)
+	public String getSomething(@QueryParam("request") String request ,
+			 @DefaultValue("1") @QueryParam("version") int version) {
+		ArrayList<User> list = userDao.getAllUsers();
+		String valor = "";
+		for (User user:list)
+		{
+			valor += user.getId()+", ";
+		}
+        return valor;	
+	}
+	
+	@GET
+	   @Path("/userId")
+	   @Produces(MediaType.APPLICATION_XML)
+	   public User getUser(@QueryParam("userid") int userid){
+		 User user = userDao.getUser(userid);
+	      return user;
+	   }
+
+	@GET
+	@Path("/addUser")
+    @Produces(MediaType.APPLICATION_XML)
+	 public ArrayList<User> createUser(@QueryParam("id") int id,
+			 @QueryParam("name") String name,
+			 @QueryParam("profession") String profession) throws IOException
+		  {
+			  ArrayList<User> users = userDao.getAllUsers();
+		      User user = new User(id, name, profession);
+		      int result = userDao.addUser(user,users);
+		      if(result == 1){
+		    	  return users;
+		         //return SUCCESS_RESULT;
+		      }
+		      
+		      return users;
+		      //return FAILURE_RESULT;*/
+		  }
+	
+	 @GET
+	 @Path("/deleteUser")
+	 @Produces(MediaType.APPLICATION_XML)
+	 public ArrayList<User> deleteUser(@QueryParam("userid") int userId)
+	 {
+		 ArrayList<User> users = userDao.getAllUsers();
+		 int resultado = userDao.deleteUser(userId,users);
+		 return users;
+	 }
+	 
+	 @GET
+	 @Path("/update")
+	 @Produces(MediaType.APPLICATION_XML)
+	 public ArrayList<User> update(@QueryParam("id") int id,
+			 @QueryParam("name") String name,
+			 @QueryParam("profession") String profession) throws IOException
+	 {
+		 ArrayList<User> users = userDao.getAllUsers();
+		 User updateUser = new User(id, name, profession);
+		 users = userDao.updateUser(updateUser,users);
+		 return users;
+	 }
+
+	
+}
